96d0a60885d7ead759666d880c6accbf98d40c2b
Salvatore Balzano <salvatore.balzano@carsguide.com.au>
2019-12-02 21:13:23 +1100

    fixed warning

--- db/db.go
+++ db/db.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Connect() *gorm.DB {
-	db, err := gorm.Open("mysql", "go-example:go-example@tcp(db:3306)/go-example")
+	db, err := gorm.Open("mysql", "go-example:go-example@tcp(db:3306)/go-example?parseTime=true")
 	if err != nil {
 		log.Fatalln(err)
 	}
